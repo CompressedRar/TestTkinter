7477d303d2da9835a053fa49f287bb67c1678f1c
KompresudoRaru <qwertythanzip1103@gmail.com>
2024-03-09 10:35:35 +0800

    modified:   __pycache__/main.cpython-312.pyc 	modified:   main.py 	new file:   static/styles/lobby.css 	new file:   templates/lobby.html

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import *
-
+from datetime import *
 app = Flask(__name__)
 app.secret_key = "hehe"
+app.permanent_session_lifetime = timedelta(minutes=30)
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -12,12 +13,30 @@ def login():
     if request.method == "POST":
         mode = request.form["mode"]
         
+        #checks what form it came from
+        # login or signin
         if mode == "login":
-            return f"{mode}"
-        else:
+            username = request.form["login-username"]
+            password = request.form["login-password"]
+
+            #checks if user is already in session    
+            #if not, authenticate
+            if "currentuser" in session:
+                return redirect(url_for("lobby", user = "HEHE"))
+
+            if username == "hehe" and password == "hehe":
+                flash("Login successful", "info")
+                session["currentuser"] = "HEHE"
+                return redirect(url_for("lobby", user = "HEHE"))
+            else: 
+                return f"Invalid account"
             
+        elif mode == "signin":
+            #signup
+            #checks if passwords match
             password = request.form["signup-password"]
             confirmpassword = request.form["signup-confirmpassword"]
+            
 
             if password != confirmpassword:
                 flash("The passwords don't match!", "info")
@@ -27,6 +46,11 @@ def login():
 
             
     return render_template("login.html",model="true")
+
+
+@app.route("/<user>")
+def lobby(user):
+    return render_template("lobby.html")
 
 if __name__ == "__main__":
     app.run()
